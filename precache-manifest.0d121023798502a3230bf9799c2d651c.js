d567ec60666478c32ec7004ff2f2e82e287edc04
wangyn <928069834@qq,com>
2019-10-14 15:35:20 +0800

    wyn

---
+++ precache-manifest.0d121023798502a3230bf9799c2d651c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0a0aee8af4fc37ecde32fd75f1bc59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5d9ee5200771696e01",
+    "url": "/static/css/main.a2b16def.chunk.css"
+  },
+  {
+    "revision": "6391ae8687e6b56aac07",
+    "url": "/static/js/2.06adc54f.chunk.js"
+  },
+  {
+    "revision": "9e5d9ee5200771696e01",
+    "url": "/static/js/main.b32dc23f.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
